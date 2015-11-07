cc9ac7fb221b123cf197a1565ab59c6775b507a1
JohnBSV <juan.bustamanteing@gmail.com>
2015-11-06 19:55:44 -0600

    Day in English was added
    There is a piece of code that if uncommented will generate graph with
    English week days for outside US users.

--- plot2.R
+++ plot2.R
@@ -14,6 +14,15 @@ Housecolnames<-lapply(Housecolnames,as.character)
 
 colnames(House)<-Housecolnames
 
+# #preserve the existing locale
+# #Those lines were used to generate png files.
+# #For users inside US should be left commented
+
+# my_locale <- Sys.getlocale("LC_ALL")
+# 
+# # change locale to English
+# Sys.setlocale("LC_ALL", "English")
+
 House$Datetime<-paste(House$Date,House$Time)
 House$Datetime<-strptime(House$Datetime,
                           format="%d/%m/%Y %T")
@@ -25,3 +34,6 @@ plot(House$Datetime,
      xlab = "",
      ylab = "Global Active Power (kilowatts)")
 dev.off()
+
+# ## restore locale
+# Sys.setlocale("LC_ALL", my_locale)
--- plot4.R
+++ plot4.R
@@ -17,16 +17,36 @@ colnames(House)<-Housecolnames
 # #preserve the existing locale
 # #Those lines were used to generate png files.
 # #For users inside US should be left commented
-# my_locale <- Sys.getlocale("LC_ALL")
-# 
-# # change locale to English
-# Sys.setlocale("LC_ALL", "English")
+#my_locale <- Sys.getlocale("LC_ALL")
+
+# change locale to English
+#Sys.setlocale("LC_ALL", "English")
 
 House$Datetime<-paste(House$Date,House$Time)
 House$Datetime<-strptime(House$Datetime,
                          format="%d/%m/%Y %T")
+png(filename = "plot4.png")
 
-png(filename = "plot3.png")
+par(mfrow=c(2,2))
+
+#Plot number 1 generation
+with(House,plot(Datetime,
+                Global_active_power,
+                type = "l",
+                xlab = "",
+                ylab = "Global Active Power (kilowatts)"
+                )
+     )
+
+#Plot number 2 generation
+with(House, plot(Datetime,
+                 Voltage,
+                 type = "l",
+                 xlab="datetime"
+                 )
+     )
+
+#Plot number 3 generation
 with(House,
      plot(Datetime,
           Sub_metering_1,
@@ -34,8 +54,9 @@ with(House,
           type = "l",
           xlab = "",
           ylab = "Energy sub metering"
-          )
+     )
 )
+
 lines(House$Datetime,
       House$Sub_metering_2,
       type = "l",
@@ -45,14 +66,26 @@ lines(House$Datetime,
       House$Sub_metering_3,
       type = "l",
       col="blue")
+
 legend("topright",
        lty = 1,
        col = c("black",
                "red",
                "blue"),
+       bty = "n",
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
 )
-dev.off()
+
+#Plot number 4 generation
+with(House,
+     plot(Datetime,
+          Global_reactive_power,
+          type = "l",
+          xlab = "datetime"
+          )
+     )
+
+dev.off()     
 
 ## restore locale
 #Sys.setlocale("LC_ALL", my_locale)
